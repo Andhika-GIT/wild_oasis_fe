36782bb273d644828b003cce38b21031cd7ab331
Andhika-GIT <andhikapr081@gmail.com>
2025-01-02 11:29:21 +0700

    feat: new action to fetch countries

---
+++ app/action/country.ts
@@ -0,0 +1,16 @@
+import { handleFetchResponse } from "@/lib/helper";
+import { Country, Error as ResponseError } from "@/types";
+
+export const getCountries = async (): Promise<Country[] | undefined> => {
+  try {
+    const response = await fetch(
+      "https://restcountries.com/v2/all?fields=name,flag"
+    );
+
+    const countries = (await response.json()) as Country[];
+    return countries;
+  } catch (e) {
+    const customError = e as ResponseError;
+    throw new Error(customError.message);
+  }
+};
