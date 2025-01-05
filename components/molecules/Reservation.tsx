6671161f458771b3b0fbe720bc543742538267ce
Andhika-GIT <andhikapr081@gmail.com>
2025-01-05 19:59:18 +0700

    feat: passing props to child component in Reservation component

--- components/molecules/Reservation.tsx
+++ components/molecules/Reservation.tsx
@@ -1,25 +1,24 @@
-import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import { getBookedDatesByCabinId } from "@/app/action/booking";
+import { getSetting } from "@/app/action/setting";
 import { DateSelector } from "./DateSelector";
 import { ReservationForm } from "./ReservationForm";
+import { Cabin } from "@/types";
+import { NextPage } from "next";
 
-async function Reservation({ cabin }) {
-  //   const [settings, bookedDates] = await Promise.all([
-  //     getSettings(),
-  //     getBookedDatesByCabinId(cabin.id),
-  //   ]);
+type ReservationProps = {
+  cabin: Cabin;
+};
+
+export const Reservation: NextPage<ReservationProps> = async ({ cabin }) => {
+  const [setting, bookedDates] = await Promise.all([
+    getSetting(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-      <DateSelector
-      // settings={settings}
-      // bookedDates={bookedDates}
-      // cabin={cabin}
-      />
-      <ReservationForm
-      //   cabin={cabin}
-      />
+      <DateSelector setting={setting} bookedDates={bookedDates} cabin={cabin} />
+      <ReservationForm cabin={cabin} />
     </div>
   );
-}
-
-export default Reservation;
+};
