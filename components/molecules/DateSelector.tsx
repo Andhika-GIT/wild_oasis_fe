f426bdcb6f0cd8602bdaecf2e96dac3bf6067588
Andhika-GIT <andhikapr081@gmail.com>
2025-01-18 20:56:49 +0700

    feat: setup zustand store for reservation dates

--- components/molecules/DateSelector.tsx
+++ components/molecules/DateSelector.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import { BookedDates, Cabin, Setting } from "@/types";
 import { isWithinInterval } from "date-fns";
-import { DayPicker } from "react-day-picker";
+import { DayPicker, DateRange } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "@/store";
 
 type DateSelectorProps = {
   cabin: Cabin | undefined;
@@ -26,12 +28,13 @@ export const DateSelector: React.FC<DateSelectorProps> = ({
   setting,
   bookedDates,
 }) => {
+  const { range, setRange, resetRange } = useReservation((state) => state);
+
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
   const numNights = 23;
   const cabinPrice = 23;
-  const range = { from: null, to: null };
 
   // SETTINGS
   const minBookingLength = setting?.min_booking_length || 1;
@@ -44,11 +47,14 @@ export const DateSelector: React.FC<DateSelectorProps> = ({
         mode="range"
         min={minBookingLength + 1}
         max={maxBookingLength}
+        onSelect={setRange}
+        selected={range}
         fromMonth={new Date()}
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        required
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
@@ -79,10 +85,10 @@ export const DateSelector: React.FC<DateSelectorProps> = ({
           ) : null}
         </div>
 
-        {range.from || range.to ? (
+        {range?.from || range?.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            // onClick={() => resetRange()}
+            onClick={() => resetRange()}
           >
             Clear
           </button>
