fe0672404257ab88b46d9a145e5e6356f8ce0e22
Andhika-GIT <andhikapr081@gmail.com>
2025-03-08 19:00:53 +0700

    feat: add new component and create new page for login & sign-up

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,71 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Label,
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Button,
+} from "@/components/ui";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Sign up",
+};
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-10 mt-10 items-center">
+      <h2 className="text-3xl font-semibold">
+        Create account to access your guest area
+      </h2>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Please all required fields below</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="Enter your username" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="Enter your email" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex flex-col space-y-3">
+          <Button variant="outline" className="w-full">
+            Submit
+          </Button>
+          <Link
+            href="/login"
+            className="hover:text-accent-400 transition-colors"
+          >
+            <Button variant="link" className="w-full">
+              Already have account ? sign in here
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
