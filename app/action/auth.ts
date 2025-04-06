307124e282539f1d7acbf974b41be6b8e9a442a7
Andhika-GIT <andhikapr081@gmail.com>
2025-04-06 21:40:23 +0700

    refactor: update response type to receive "success" response from backend

--- app/action/auth.ts
+++ app/action/auth.ts
@@ -1,20 +1,28 @@
 import { SERVER_BASE_URL, handleFetchResponse } from "@/lib/helper";
-import { Cabin, Error as ResponseError } from "@/types";
+import { ApiResponse, Cabin, Error as ResponseError } from "@/types";
 import { AuthSignInRequestBody } from "@/types";
 
-export const signIn = async ( signInBody : AuthSignInRequestBody): Promise<string | undefined> => {
+type SignInResponse = ApiResponse<string>;
+
+
+export const signIn = async (
+  formData: AuthSignInRequestBody
+): Promise<SignInResponse> => {
   const BASE_URL = `${SERVER_BASE_URL}/auth/sign-in`;
 
   try {
     const response = await fetch(BASE_URL, {
-      method: 'POST',
-      body: JSON.stringify(signInBody),
+      method: "POST",
+      body: JSON.stringify(formData),
     });
 
-    return await handleFetchResponse<string>(response);
+    // return await handleFetchResponse<string>(response);
+    const result: ApiResponse<string> = await response.json();
+
+    return result
   } catch (e) {
     const customError = e as ResponseError;
-    throw new Error(customError.message);
+    return customError
   }
 };
 
@@ -24,12 +32,12 @@ export const signIn = async ( signInBody : AuthSignInRequestBody): Promise<strin
 //     const BASE_URL = maxCapacityFilter
 //       ? `${SERVER_BASE_URL}/cabins?max_capacity=${maxCapacityFilter}`
 //       : `${SERVER_BASE_URL}/cabins`;
-  
+
 //     try {
 //       const response = await fetch(BASE_URL, {
 //         cache: "no-cache",
 //       });
-  
+
 //       return await handleFetchResponse<Cabin[]>(response);
 //     } catch (e) {
 //       const customError = e as ResponseError;
