ecfb213bd52b4d9307e662c51b38f5308e65c315
Andhika-GIT <andhikapr081@gmail.com>
2025-05-24 19:53:59 +0700

    feat: add new action to get current user bookings & update booking types

--- app/action/booking/server.ts
+++ app/action/booking/server.ts
@@ -1,6 +1,7 @@
 import { SERVER_BASE_URL, handleFetchResponse } from "@/lib/helper";
-import { BookedDates, Error as ResponseError } from "@/types";
+import { BookedDates, Booking, Error as ResponseError } from "@/types";
 import { notFound } from "next/navigation";
+import { cookies } from "next/headers";
 
 export const getBookedDatesByCabinId = async (
   id: number | undefined
@@ -11,6 +12,29 @@ export const getBookedDatesByCabinId = async (
     );
 
     return await handleFetchResponse<BookedDates>(response);
+  } catch (e) {
+    const customError = e as ResponseError;
+    if (customError.code === 404) {
+      notFound();
+    } else {
+      throw new Error(customError.message);
+    }
+  }
+};
+
+export const getCurrentUserBookings = async (): Promise<
+  Booking | undefined
+> => {
+  try {
+    const response = await fetch(`${SERVER_BASE_URL}/booking/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: cookies().toString(),
+      },
+    });
+
+    return await handleFetchResponse<Booking>(response);
   } catch (e) {
     const customError = e as ResponseError;
     if (customError.code === 404) {
--- types/booking.ts
+++ types/booking.ts
@@ -11,4 +11,20 @@ export type BookingReservation = {
   cabins: { name: string; image: string };
 };
 
+export type Booking = {
+  id: number;
+  start_date: string;     // ISO 8601 format (timestamp string)
+  end_date: string;
+  num_nights: number;
+  num_guests: number;
+  cabin_price: number;
+  extras_price: number;
+  total_price: number;
+  status: string;
+  has_breakfast: boolean;
+  is_paid: boolean;
+  observations: string;
+  cabin_id: number;
+};
+
 export type BookedDates = string[];
