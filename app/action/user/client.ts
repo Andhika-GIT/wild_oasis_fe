4196d7fad2badf7b03ee94dcb40d77a616ec18f7
Andhika-GIT <andhikapr081@gmail.com>
2025-06-07 19:20:27 +0700

    feat: implement sign-out

--- app/action/user/client.ts
+++ app/action/user/client.ts
@@ -1,25 +1,45 @@
-'use client'
+"use client";
 
-import { handleFetchResponse, SERVER_BASE_URL } from "@/lib/helper"
-import { Error } from "@/types"
-import { updateNationalitySchemaType } from "@/lib/schemas/update-nationality"
+import { handleFetchResponse, SERVER_BASE_URL } from "@/lib/helper";
+import { Error } from "@/types";
+import { Error as ResponseError } from "@/types";
+import { updateNationalitySchemaType } from "@/lib/schemas/update-nationality";
 
-export const UpdateCurrentUserNationality = async ( formData: updateNationalitySchemaType) : Promise<string | undefined> => {
-    const BASE_URL = `${SERVER_BASE_URL}/auth/update-nationality`
+export const UpdateCurrentUserNationality = async (
+  formData: updateNationalitySchemaType
+): Promise<string | undefined> => {
+  const BASE_URL = `${SERVER_BASE_URL}/auth/update-nationality`;
 
-    try {
-        const response = await fetch(BASE_URL, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-            credentials: 'include'
-        })
+  try {
+    const response = await fetch(BASE_URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+      credentials: "include",
+    });
 
-        return await handleFetchResponse(response)
-    }
-    catch(e) {
-        throw e as Error
-    }
-}
+    return await handleFetchResponse(response);
+  } catch (e) {
+    throw e as Error;
+  }
+};
+
+export const signOutUser = async (): Promise<String | undefined> => {
+  const BASE_URL = `${SERVER_BASE_URL}/auth/sign-out`;
+
+  try {
+    const response = await fetch(BASE_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    return await handleFetchResponse(response);
+  } catch (e) {
+    throw e as ResponseError;
+  }
+};
--- components/molecules/SignOutButton.tsx
+++ components/molecules/SignOutButton.tsx
@@ -1,12 +1,49 @@
+"use client";
+
 import { ArrowRightCircleIcon } from "@heroicons/react/20/solid";
-import { Button } from "../ui";
+import { Button, LoadingSpinner } from "../ui";
 import { NextPage } from "next";
+import { useMutation } from "@tanstack/react-query";
+import { Error as ApiError } from "@/types";
+import { signOutUser } from "@/app/action/user";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { TrashIcon } from "@radix-ui/react-icons";
 
 export const SignOutButton: NextPage = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const { mutate } = useMutation({
+    mutationFn: () => {
+      setIsLoading(true);
+      return signOutUser();
+    },
+    onSuccess: () => {
+      toast.success("Sucessfully sign out");
+      setIsLoading(false);
+      router.refresh();
+      router.push("/");
+    },
+    onError: (error: ApiError) => {
+      setIsLoading(false);
+      toast.error(error.message || "something went wrong");
+    },
+  });
+
   return (
-    <Button className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full">
-      <ArrowRightCircleIcon className="h-5 w-5 text-primary-600" />
-      <span>Sign out</span>
+    <Button className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full" disabled={isLoading} onClick={() => mutate()}>
+      {!isLoading ? (
+        <>
+          <ArrowRightCircleIcon className="h-5 w-5 text-primary-600" />
+          <span>Sign out</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <LoadingSpinner />
+        </span>
+      )}
     </Button>
   );
 };
